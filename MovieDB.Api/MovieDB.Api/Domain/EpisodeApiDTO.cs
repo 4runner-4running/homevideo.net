b8ee2bbe1e5fb44d85d1757ecb8f7f1d8cc56808
james <wjhguitarman@gmail.com>
2021-03-02 20:32:39 -0600

    Added tv, season, and episode api dto classes. Added GetTvShow method. Updated ParseStatusCode to better reflect expected results from api

---
+++ MovieDB.Api/MovieDB.Api/Domain/EpisodeApiDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDB.Api.Domain
+{
+    public class EpisodeApiDTO
+    {
+        public string Air_Date { get; set; }
+        public int Episode_Number { get; set; }
+        public string Name { get; set; }
+        public string Overview { get; set; }
+        public string Production_Code { get; set; }
+        public int Season_Number { get; set; }
+        public string Still_Path { get; set; }
+        public decimal Vote_Average { get; set; }
+        public int Vote_Count { get; set; }
+    }
+}
